718c8dee5eee17eaaf80479bcc91e3d5f04b632b
charish37 <charishmanadipalli77@gmail.com>
2025-04-28 18:21:17 -0400

    add count digit optimal solution

--- BasicMathPbms/CountDigit.java
+++ BasicMathPbms/CountDigit.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class CountDigit{
 
     // brute force approach
-    public  static int countVal(int num){
-      int count = 0;
-       while(num > 0){
-        count++;
-        num = num / 10;
-       }
-      return count;
+    // public  static int countVal(int num){
+    //   int count = 0;
+    //    while(num > 0){
+    //     count++;
+    //     num = num / 10;
+    //    }
+    //   return count;
+    // }
+
+    // optimal sol
+    public static int countVal(int num){
+      return (int) Math.log10(num) + 1;
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
