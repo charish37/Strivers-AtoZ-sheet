b6944171a67db8230f7447ca5d4c0012f86130dc
charish37 <charishmanadipalli77@gmail.com>
2025-04-29 12:55:22 -0400

    improve performance to reverse numbers

--- BasicMathPbms/ReverseNumbers.java
+++ BasicMathPbms/ReverseNumbers.java
@@ -2,16 +2,28 @@ import java.util.Arrays;
 
 public class ReverseNumbers {
 
-    public static int[] PrintReverseArray(int[] arr,int i, int j){
-       if(i >= j) return arr;
-       int temp = arr[i];
-       arr[i] = arr[j];
-       arr[j] = temp;
-       return PrintReverseArray(arr, i+1, j-1);
+    // public static int[] PrintReverseArray(int[] arr,int i, int j){
+    //    if(i >= j) return arr;
+    //    int temp = arr[i];
+    //    arr[i] = arr[j];
+    //    arr[j] = temp;
+    //    return PrintReverseArray(arr, i+1, j-1);
+
+    // improve performance by using loops instead of recursion
+
+    public static int[] PrintReverseArray(int[] arr){
+        
+        int len = arr.length-1;
+        int i=0,j=len;
+       while(i < j){
+         int temp = arr[i];
+         arr[i++] = arr[j];
+         arr[j--] = temp;
+       }
+       return arr;
     }
     public static void main(String args[]){
         int[] arr = new int[] {1,2,3,4,5};
-        int len = arr.length-1;
-        System.out.println(Arrays.toString(PrintReverseArray(arr,0,len)));
+        System.out.println(Arrays.toString(PrintReverseArray(arr)));
     }
 }
